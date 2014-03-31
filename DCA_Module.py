c35b5b578586c94374e8e210e8bdb3112e76d145
Derek Kaknes <derekkaknes@Dereks-MacBook-Pro.local>
2014-03-31 09:07:24 -0400

    Additional Refactoring

--- DCA_Module.py
+++ DCA_Module.py
@@ -3,20 +3,16 @@ import numpy.linalg as npl
 import random as rand
 import sys
 import csv
+from math import *
+import time
 
 
 # Instantiate Constants
-AA_out='0'
-AA_matrix=['+','-']
-x=0.7
-q=3
-
-
 
 #######  FUNCTION IMPLEMENTATIONS ##########
 
 def load_txt_list(filename, dtype = "str"):
-    print "Executing load_txt_list"
+    print "Executing load_txt_list..."
     raw_text_input = np.loadtxt(filename, dtype)
     line_parsed_list = []
     for seq in raw_text_input:
@@ -27,7 +23,7 @@ def load_txt_list(filename, dtype = "str"):
 
 
 def single_residue_freq(concat_matrix, AA_matrix, q, AA_out, km_matrix):
-  print "Executing single_residue_freq"
+  print "Executing single_residue_freq..."
 
   [M,L] = np.shape(concat_matrix)
   Meff = sum(km_matrix)
@@ -67,32 +63,40 @@ def single_residue_freq(concat_matrix, AA_matrix, q, AA_out, km_matrix):
   fiAB=fiA_vector1 * fiA_vector2
   return [fiA_matrix_matrix, fiAB, fiA_vector1, fiA_out_list]
 
-def residue_pair_freq(concat_matrix, km_matrix):
-  print "Executing residue_pair_freq"
+def residue_pair_freq(concat_matrix, km_matrix, AA_matrix, AA_out):
+  print "Executing residue_pair_freq..."
   [M,L] = np.shape(concat_matrix)
   Meff = sum(km_matrix)
   lam = Meff
 
   fijAB_matrix=np.zeros((2*L,2*L))
-
-  for i in range(L):
-    for j in range(L):
-      for k in range(M):
-        i_val = concat_matrix[k][i]
-        j_val = concat_matrix[k][j]
-        if i_val != AA_out and j_val != AA_out:
-          i_index = AA_matrix.index(i_val)
-          j_index = AA_matrix.index(j_val)
-          pair_address_x = (i) * 2 + i_index
+  print "Sum of frequency matrix = ", sum(sum(fijAB_matrix))
+  time1 = time.time()
+  for i in range(20):
+    time2 = time.time()
+    diff = time2 - time1
+    time1 = time2
+    print "Executed residue ",i," in ",diff," seconds"
+    for j in range(15):
+      for day in range(10): # M is very long, 3801 items, reps the number of trading day data points
+        i_return = concat_matrix[day][i] # the return (+,-,0) of company i on day k
+        j_return = concat_matrix[day][j] # the return (+,-,0) of company j on day k
+        if i_return != AA_out and j_return != AA_out: # if the return of CompA and CompB is non-zero, then:
+          i_index = AA_matrix.index(i_return)  # i_index is the return value (+,-) - could later include larger range of returns
+          j_index = AA_matrix.index(j_return)
+          pair_address_x = (i) * 2 + i_index # paX equals double the company index (i) plus the amino acid index
           pair_address_y = (j) * 2 + j_index
-          fijAB_matrix[pair_address_y][pair_address_x] += km_matrix[k]
+          fijAB_matrix[pair_address_y][pair_address_x] += km_matrix[day]
+          print "Companies (j,i, day) ",[j,i,day], "Pair Address: ",[pair_address_y, pair_address_x]," Returns: ",[j_return, i_return], " fijAB value: ",fijAB_matrix[pair_address_y][pair_address_x]
+          print "Sum of frequency matrix = ", sum(sum(fijAB_matrix))
+          time.sleep(0.5)
 
   fijAB_matrix = fijAB_matrix + (lam / float(q**2))
   fijAB_matrix = fijAB_matrix / float(lam + Meff)
   return fijAB_matrix
 
 def generate_covariance_matrix(fijAB_matrix, fiAB, fiA_matrix_matrix):
-  print "Executing generate_covariance_matrix"
+  print "Executing generate_covariance_matrix..."
   L = len(fiAB)/2
   Cij_matrix = fijAB_matrix - fiAB
 
@@ -105,7 +109,106 @@ def generate_covariance_matrix(fijAB_matrix, fiAB, fiA_matrix_matrix):
   return Cij_matrix
 
 def generate_coupling_energies(Cij_matrix):
-  print "Executing generate_coupling_energies"
+  print "Executing generate_coupling_energies..."
   eij_matrix1 = npl.inv(Cij_matrix)
   eij_matrix = -eij_matrix1
   return eij_matrix
+
+def join_fiA(fiA, fiA_out):
+  # This function takes in fiA and fiA_out and returns a new array, fiA_list_list,
+  # which combines the input arrays in the pattern of [fiA[0], fiA[1], fiA_out[0]]
+  print "Executing join_fiA..."
+  result = []
+  for i in range(len(fiA_out)):
+      for j in range(2):
+          result.append(fiA[2*i+j])
+      result.append(fiA_out[i])
+  return result
+
+
+def eij_matrix(eij, i, j, q):
+  # This function takes the (2L x 2L) matrix eij and maps to a qxq matrix equal to: 
+  # [[e00,e01,1],
+  #  [e10,e11,1],
+  #  [1,1,1]]
+
+  result = []
+  for k in range(2):
+      eij_list=[]
+      for m in range(2):
+          eij1 = exp(eij[2*i+k][2*j+m])
+          #print "eij[",2*i+k,"][",2*j+m,"] = ",eij1,": i,j = ",i,",",j
+          eij_list.append(eij1)
+      # Set last column to 1
+      eij_list.append(1)
+      result.append(eij_list)
+  # Set last row to 1's
+  eij_out = [1]*q
+  result.append(eij_out)
+  return np.array(result)
+
+def local_biases(pi,pj,W, epsilon = 0.0001):
+  #This function takes pi, pj and W and returns the local_biases hiA and hiB
+  # THIS FUNCTION IS THE PRIMARY COMPUTATIONAL EXPENSE
+  q = len(pi)
+  mu1 = np.ones(q)/q
+  mu2 = np.ones(q)/q
+  diff = 1
+
+  while diff > epsilon:
+      
+      scra1 = np.dot(mu2,np.transpose(W))
+      scra2 = np.dot(mu1,W)
+
+      new1 = pi/scra1
+      new1 = new1/sum(new1)
+
+      new2 = pj/scra2
+      new2 = new2/sum(new2)
+
+      sub_diff1 = new1 - mu1
+      sub_diff2 = new2 - mu2
+
+      diff = max(max(np.absolute(sub_diff1)), max(np.absolute(sub_diff2)))
+      
+      mu1 = new1
+      mu2 = new2
+
+  hiA = mu1
+  hiB = mu2
+  return [hiA, hiB]
+
+def individual_DI(fiA_list_list, pdir,i,j,q):
+  DI = 0
+  for k in range(q):
+      for m in range(q):
+          pdir_sub = pdir[k][m]
+          DI += pdir_sub*log(pdir_sub/float(fiA_list_list[(i)*q + k]*fiA_list_list[(j)*q + m]))
+
+  return DI
+
+def direct_information(L,q,eij, fiA_list_list):
+  print "Executing direct_information..."
+  DI_matrix = []
+
+  for i in range(L):
+      DI_list = []
+      for j in range(L):
+
+          W = eij_matrix(eij,i,j,q)
+
+          pi = np.array(fiA_list_list[(i)*q:(i)*q + q])
+          pj = np.array(fiA_list_list[(j)*q:(j)*q + q])
+
+          [hiA, hiB] = local_biases(pi,pj,W)
+
+          pdir = W*(np.reshape(hiA, (q, 1))*hiB)
+          pdir = pdir / pdir.sum()
+
+          DI = individual_DI(fiA_list_list, pdir,i,j,q)
+          DI_list.append(DI)
+
+      DI_matrix.append(DI_list)
+
+  return DI_matrix
+
--- DCA_runfile.py
+++ DCA_runfile.py
@@ -1,41 +1,83 @@
 from DCA_Module import *
 import numpy as np
-import numpy.linalg as npl
-import random as rand
-import sys
-import csv
+import time
 
-# Import Data
-[concat_list, concat_matrix, concat_list_raw] = load_txt_list('sequence_list_abbrev')
+global_start = time.time()
 
-# Instantiate Constants
-AA_out='0'
-AA_matrix=['+','-']
-x=0.7
-q=3
-[M,L] = np.shape(concat_matrix)
+# Generate covariance matrix?  
+generate_Cij = "Yes"
 
-# IN FUTURE INSERT Km IMPLEMENTATION
-km_matrix = np.array([1]*M)
-# IN FUTURE INSERT Km IMPLEMENTATION 
+if generate_Cij == "Yes":
 
-# Calculate single frequencies (fiA)
-[fiA_matrix_matrix, fiAB, fiA_vector1, fiA_out_list] = single_residue_freq(concat_matrix, AA_matrix, q, AA_out, km_matrix)
+  # Import Data
+  ts = time.time()
+  [concat_list, concat_matrix, concat_list_raw] = load_txt_list('sequence_list')
+  runtime = time.time() - ts
+  print "Executed in ", runtime, "seconds"
 
-# Calculate residue pair frequencies (fijAB)
-fijAB_matrix = residue_pair_freq(concat_matrix, km_matrix)
+  # Instantiate Constants
+  AA_out='0'
+  AA_matrix=['+','-']
+  x=0.7
+  q = len(AA_matrix) + len(AA_out)
+  [M,L] = np.shape(concat_matrix)
 
-# Generate covariance matrix
-Cij_matrix = generate_covariance_matrix(fijAB_matrix, fiAB, fiA_matrix_matrix)
+  # IN FUTURE INSERT Km IMPLEMENTATION
+  km_matrix = np.array([1]*M)
+  # IN FUTURE INSERT Km IMPLEMENTATION 
 
-# Generate coupling energies
-eij_matrix = generate_coupling_energies(Cij_matrix)
+  # Calculate single frequencies (fiA)
+  ts = time.time()
+  [fiA_matrix_matrix, fiAB, fiA_vector1, fiA_out_list] = single_residue_freq(concat_matrix, AA_matrix, q, AA_out, km_matrix)
+  runtime = time.time() - ts
+  print "Executed in ", runtime, "seconds"
 
-# Save relevant output files
-np.savetxt('stock_km_matrix', km_matrix)
-np.savetxt('stocks_fiA', fiA_vector1)
-np.savetxt('stocks_fiA_out', fiA_out_list)
-np.savetxt('stocks_eij', eij_matrix)
-np.savetxt('Covariant Matrix', Cij_matrix)
+  # Calculate residue pair frequencies (fijAB)
+  ts = time.time()
+  fijAB_matrix = residue_pair_freq(concat_matrix, km_matrix, AA_matrix, AA_out)
+  runtime = time.time() - ts
+  print "Executed in ", runtime, "seconds"
 
-print "End of program"
+  # Generate covariance matrix
+  ts = time.time()
+  Cij_matrix = generate_covariance_matrix(fijAB_matrix, fiAB, fiA_matrix_matrix)
+  runtime = time.time() - ts
+  print "Executed in ", runtime, "seconds"
+
+  # Generate coupling energies
+  ts = time.time()
+  eij_matrix = generate_coupling_energies(Cij_matrix)
+  runtime = time.time() - ts
+  print "Executed in ", runtime, "seconds"
+
+  # Save relevant output files
+  np.savetxt('stock_km_matrix', km_matrix)
+  np.savetxt('stocks_fiA', fiA_vector1)
+  np.savetxt('stocks_fiA_out', fiA_out_list)
+  np.savetxt('stocks_eij', eij_matrix)
+  np.savetxt('Covariant Matrix', Cij_matrix)
+
+else:
+  fiA_vector1 = np.loadtxt('stocks_fiA')
+  eij_matrix = np.loadtxt('stocks_eij')
+  fiA_out_list = np.loadtxt('stocks_fiA_out')
+  q = 3
+  L = len(fiA_vector1)/2
+
+
+# Reorganize fiA and fiA_out_list
+ts = time.time()
+fiA_matrix = join_fiA(fiA_vector1, fiA_out_list)
+runtime = time.time() - ts
+print "Executed in ", runtime, "seconds"
+
+# Calc DI for each residue
+ts = time.time()
+DI_matrix = direct_information(L,q,eij_matrix,fiA_matrix)
+runtime = time.time() - ts
+print "Executed in ", runtime, "seconds"
+
+np.savetxt('fiA_list_list', fiA_matrix)
+np.savetxt('stocks_DI_scores', DI_matrix)
+
+print "End of program - executed in ", time.time() - global_start, "seconds"
